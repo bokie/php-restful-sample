42f3237a5f7704ed27df54169f49bbb59faa66a0
bokie <yyybokie@gmail.com>
2017-12-11 17:00:16 +0800

    定义错误代码

---
+++ lib/ErrorCode.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 错误代码配置文件
+ */
+
+class ErrorCode
+{
+    const USERNAME_EXISITS = 1; // 用户名已存在
+    const PASSWORD_EMPTY = 2; // 用户密码不能为空
+    const USERNAME_EMPTY = 3; // 用户名不能为空
+    const REGISTER_FAIL = 4; // 注册失败
+    const LOGIN_FAIL = 5; // 登录失败
+}
